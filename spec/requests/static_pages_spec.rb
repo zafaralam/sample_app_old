807bf4f89bea4235b81c9c0fb3851ea45373828e
Syed Zafar Alam <zmacmini@Syeds-Mac-mini.local>
2012-08-11 21:21:13 +1000

    Added the contact page to the static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home pages" do
 
     it "should work the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right tile" do
       visit '/static_pages/home'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title',:text => "#{base_title} | Home")
     end
   end
 
@@ -22,7 +24,7 @@ describe "Static pages" do
    end
    it "should have the right tile" do
       visit '/static_pages/help'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title',:text => "#{base_title} | Help")
     end
   end
 
@@ -34,7 +36,19 @@ describe "Static pages" do
 
     it "should have the right tile" do
       visit '/static_pages/about'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | About")
+      page.should have_selector('title',:text => "#{base_title} | About")
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the right tile" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',:text => "#{base_title} | Contact")
     end
   end
 end
